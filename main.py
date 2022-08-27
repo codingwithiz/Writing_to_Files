3eaf2a52f76d08717b9fd71a5cee36506c0d4a0e
Ing Zhen Lee <ingzhen2003@gmail.com>
2022-08-27 14:25:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+employee_file = open("Amelia Task 12.txt" , "w")
+
+'''
+w: write - will overwrite any existing content
+a: append - will append to the end of the file
+'''
+
+employee_file.write("\nKelly - Customer Service")
+
+employee_file.close()
+
